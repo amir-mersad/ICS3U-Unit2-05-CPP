a9068697c59ffcad800c2aee90f3aaff12a375f1
amir-mersad <54957019+amir-mersad@users.noreply.github.com>
2019-09-24 20:45:42 -0400

    program added

---
+++ unit2-05.cpp
@@ -0,0 +1,38 @@
+// Copyright (c) 2019 Amirmahdi Mersad All rights reserved
+//
+// Created by Amir Mersad
+// Created on September 2019
+// This program shows how local and global variables work
+
+#include <iostream>
+
+// global variable
+int variableX = 25;
+
+void localVariable() {
+    // this shows what happens with local variables
+
+    int variableX = 10;
+    int variableY = 30;
+    int variableZ = variableX + variableY;
+    std::cout << "Local variableX, variableY, variableX: " << variableX
+              << " + " << variableY << " = " << variableZ << std::endl;
+}
+
+
+void globalVariable() {
+    // this shows what happens with global variables
+
+    variableX = variableX + 1;
+    int variableY = 30;
+    int variableZ = variableX + variableY;
+    std::cout << "Local VariableX, variableY, variableZ: " << variableX
+              << " + " << variableY << " = " << variableZ << std::endl;
+}
+
+int main() {
+    // this function calls local and global
+
+    localVariable();
+    globalVariable();
+}
